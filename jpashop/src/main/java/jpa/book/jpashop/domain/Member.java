06537205fc56f8481c309712372177879d20f188
gusrb8925 <gusrb8925@naver.com>
2021-08-02 22:09:04 +0900

    21-08-02 실전 예제 1

---
+++ jpashop/src/main/java/jpa/book/jpashop/domain/Member.java
@@ -0,0 +1,60 @@
+package jpa.book.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
---
+++ jpashop/src/main/java/jpa/book/jpashop/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package jpa.book.jpashop.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
