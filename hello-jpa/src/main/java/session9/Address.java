9d2d0dccc33e99f9231c2341ecff135db3e7fc62
gusrb8925 <gusrb8925@naver.com>
2021-08-05 17:04:59 +0900

    21-08-05 값 타입

---
+++ hello-jpa/src/main/java/session9/Address.java
@@ -0,0 +1,63 @@
+package session9;
+
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
+// 임베디드 타입은 불변 객체로 만들자
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    // 기본 생성자 필수
+    public Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    // 불변
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    // 불변
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    // 불변
+    private void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
---
+++ hello-jpa/src/main/java/session9/JpaMain.java
@@ -0,0 +1,51 @@
+// 프록시 객체는 처음 사용할 때 한번만 초기화
+// 프록시 객체를 초기화 할때, 프로식 객체가 실제 엔티티 클래스로 바뀌는 것이 아님.
+// 초기화 되면 프록시 객체를 통해서 접근 가능
+
+// 타입 비교는 ==으로 하면 안됨!! instance of 사용
+
+// 영속성 컨텍스트에 찾는 엔티티가 이미 있으면 getReference()를 호출해도 실제 엔티티 반환
+// 영속성 컨텍스트에 프록시가 있으면 프록시 반환
+
+// 프록시는 영속성 컨텍스트를 통하여 초기화한다 > 오류 조심
+package session9;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        // EntityManagerFactory 싱글톤으로 생성
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // persistence.xml
+
+        EntityManager entityManager = emf.createEntityManager();
+
+        // jpa 모든 데이터 변경은 트랜잭션 안에서 실행
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("aaa");
+            member.setHomeAddress(new Address("city", "street", "zipcode"));
+
+            member.getFavoriteFoods().add("라면");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("치킨");
+
+            entityManager.persist(member);
+
+            entityTransaction.commit();
+        } catch (Exception e) {
+            entityTransaction.rollback();
+        } finally {
+            entityManager.close();
+        }
+
+        emf.close();
+    }
+}
