52e17fece5472f324e22ef4408c5d47de86e6764
gusrb8925 <gusrb8925@naver.com>
2021-08-06 20:47:55 +0900

    21-08-06 프로젝트 환경설정

---
+++ jpaapp1/src/main/java/com/example/jpaapp1/Member.java
@@ -0,0 +1,20 @@
+package com.example.jpaapp1;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+}
---
+++ jpaapp1/src/test/java/com/example/jpaapp1/MemberRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.example.jpaapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+//    @Rollback(value = false) // Test 클래스에 @Transactional 있으면 롤백함
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getName()).isEqualTo(member.getName());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
