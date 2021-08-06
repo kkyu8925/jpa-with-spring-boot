97eb19a8b7c0eb3b30beae166effe3014b4d8022
gusrb8925 <gusrb8925@naver.com>
2021-08-06 18:44:10 +0900

    21-08-06 init

---
+++ jpaapp1/src/main/java/com/example/jpaapp1/Jpaapp1Application.java
@@ -0,0 +1,13 @@
+package com.example.jpaapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpaapp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpaapp1Application.class, args);
+    }
+
+}
---
+++ jpaapp1/src/test/java/com/example/jpaapp1/Jpaapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpaapp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
