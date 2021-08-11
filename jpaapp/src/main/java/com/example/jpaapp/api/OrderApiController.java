afd8f9ba4f2caf43f32a9ff1bb9bb1a266fea774
gusrb8925 <gusrb8925@naver.com>
2021-08-11 21:47:35 +0900

    21-08-11 API 개발 고급 - 컬렉션 조회 최적화

--- jpaapp/src/main/java/com/example/jpaapp/api/OrderApiController.java
+++ jpaapp/src/main/java/com/example/jpaapp/api/OrderApiController.java
@@ -22,11 +22,11 @@ import static java.util.stream.Collectors.*;
 
 /**
  * 권장 순서
- * 1. 엔티티 조회 방식으로 우선 접근
+ * 1. 엔티티 조회 방식으로 우선 접근 v2
  * * 1-1. 페치조인으로 쿼리 수를 최적화
- * * 1-2. 컬렉션 최적화
- * * * 1-2-1. 페이징 필요 hibernate.default_batch_fetch_size , @BatchSize 로 최적화
- * * * 1-2-2. 페이징 필요X 페치 조인 사용
+ * * 1-2. 컬렉션 최적화 v3, v3.1
+ * * * 1-2-1. 페이징 필요 hibernate.default_batch_fetch_size , @BatchSize 로 최적화 v3.1
+ * * * 1-2-2. 페이징 필요X 페치 조인 사용 v3
  * 2. 엔티티 조회 방식으로 해결이 안되면 DTO 조회 방식 사용
  * 3. DTO 조회 방식으로 해결이 안되면 NativeSQL or 스프링 JdbcTemplate
  */
