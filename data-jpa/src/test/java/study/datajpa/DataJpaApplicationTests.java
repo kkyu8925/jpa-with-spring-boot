1034483a98d06156e3108fc8cbc4f633f985cd34
gusrb8925 <gusrb8925@naver.com>
2021-08-12 19:18:58 +0900

    21-08-12 init

---
+++ data-jpa/src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
